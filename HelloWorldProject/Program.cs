e58558791e46597c66036cf1a69405aab70b822c
Mitchell Combs <mitch.combs.92@gmail.com>
2018-05-30 08:44:30 -0400

    Initial Hello World exercise commit

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorldProject
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
+namespace HelloWorldProject {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello, World!");
+
+			Student student = new Student();
+			student.SetFirstname("Frank");
+			student.SetLastname("Castle");
+			student.SetStudentId(123);
+			student.SetGPA(3.7);
+			student.SetSAT(750);
+			student.SetIsFemale(false);
+
+			string fname;
+			string lname;
+			int Sid;
+			double GPA;
+			int SAT;
+			bool Gender;
+
+			fname = student.GetFirstname();
+			lname = student.GetLastname();
+			Sid = student.GetStudentId();
+			GPA = student.GetGPA();
+			SAT = student.GetSAT();
+			Gender = student.GetIsFemale();
+
+			Console.WriteLine(fname + " " + lname + " " + Sid + " " + GPA + " " + SAT + " " + Gender);
+
 		}
 	}
 }
---
+++ HelloWorldProject/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProject
+{
+	class Student
+	{
+		string Firstname;
+		string Lastname;
+		int StudentId;
+		double GPA;
+		int SAT;
+		bool IsFemale;
+
+
+		public string GetFirstname()
+		{
+			return Firstname;
+		}
+		public void SetFirstname(string pFirstname)
+		{
+			Firstname = pFirstname;
+
+		}
+
+		public void SetLastname(string pLastname)
+		{
+			Lastname = pLastname;
+		}
+
+		public string GetLastname()
+		{
+			return Lastname;
+		}
+
+		public int GetStudentId()
+		{
+			return StudentId;
+		}
+
+		public void SetStudentId(int pStudentId)
+		{
+			StudentId = pStudentId;
+		}
+
+		public double GetGPA()
+		{
+			return GPA;
+		}
+
+		public void SetGPA(double pGPA)
+		{
+			GPA = pGPA;
+		}
+
+		public int GetSAT()
+		{
+			return SAT;
+		}
+
+		public void SetSAT(int pSAT)
+		{
+			SAT = pSAT;
+		}
+
+		public bool GetIsFemale()
+		{
+			return IsFemale;
+		}
+
+		public void SetIsFemale(bool pIsFemale)
+		{
+			IsFemale = pIsFemale;
+		}
+	}
+}
